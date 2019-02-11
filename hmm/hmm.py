ef7a2bb1b2d9d300d5e61ddc680234883dbf3e07
Abhisek Jana <adeveloperdiary@gmail.com>
2019-02-10 20:51:57 -0500

    Logistic Regression

--- hmm/hmm.py
+++ hmm/hmm.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import numpy as np
 
-data=pd.read_csv('hmm/Coding3_HMM_Data.csv')
+data = pd.read_csv('hmm/Coding3_HMM_Data.csv')
 
-W=data['Z'].values
-X=data['X'].values
+W = data['Z'].values
+X = data['X'].values
 
+A = np.array(((0.54, 0.46), (0.49, 0.51)))
+B = np.array(((0.16, 0.26, 0.58), (0.25, 0.28, 0.47)))
+
+initial_distribution = np.array((0.5, 0.5))
