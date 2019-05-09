13721fb74058de2d1e6f60ff4c0a2d85ce757269
adeveloperdiary <adeveloperdiary@gmail.com>
2019-05-08 21:46:01 -0400

    sobel

---
+++ B_Vision/C_Theory_Implementations/sobel.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+cv2.imshow("Original", image)
+cv2.waitKey(0)
